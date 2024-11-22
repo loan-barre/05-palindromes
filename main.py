395e8f9e9ba7b7dc4db89e0fd495fdf7adbffedc
loan-barre <loan.barre@edu.esiee.fr>
2024-11-22 12:53:27 +0000

    Trop cool ce code (non)

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-#### Fonction secondaire
-
+"""
+LOAN BARRE 22/11/2024 - ESIEE PARIS 
+Code permettant de trouver les palindromes
+"""
 
 def ispalindrome(p):
+    """
+    Permet de déterminer si un mot est un palindrome
+    """
 
-    # votre code ici
-    
-    return False
+    table = str.maketrans("éèêëàâùôîïç", "eeeeaauoiic", "-,'!?;.:§%*_& ")
+    p = p.lower()
+    p = p.translate(table)
+    i = 0
+    j=len(p)-1
+    while i<j:
+        if p[i]!=p[j]:
+            return False
+        i+=1
+        j-=1
+    return True
+
 
 #### Fonction principale
 
 
 def main():
+    """
+    Permert l'utilisation de la méthode ispalindrome
+    """
 
     # vos appels à la fonction secondaire ici
-
+    print("lol", ispalindrome("lol"))
+    print("caca", ispalindrome("caca"))
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
 
